b9d1d610793f7423da0f4ab5315628f9eb70e45b
efremovadiana <136587564+efremovadiana@users.noreply.github.com>
2026-03-14 18:18:37 +0300

    Create comparators.h

---
+++ comparators.h
@@ -0,0 +1,13 @@
+#ifndef COMPARATORS_H_INCLUDED
+#define COMPARATORS_H_INCLUDED
+
+// Сравнение для целых чисел
+int cmp_int(const void *a, const void *b);
+
+// Сравнение для вещественных чисел
+int cmp_double(const void *a, const void *b);
+
+// Сравнение для строк
+int cmp_str(const void *a, const void *b);
+
+#endif // COMPARATORS_H_INCLUDED
